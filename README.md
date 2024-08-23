dd2758d6346d9ee6aec37b86fb47a59191203545
zhouyi <shzy2012@gmail.com>
2024-08-23 23:59:36 +0800

    .

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_vscode_layout_example
+
+```
+flutter run
+```
---
+++ android/app/src/main/kotlin/com/example/flutter_vscode_layout_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_vscode_layout_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
